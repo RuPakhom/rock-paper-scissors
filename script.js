0d353ff5b37c0d78ff11c5ea0c4f3a7601b68dff
RuPakhom <197668456+RuPakhom@users.noreply.github.com>
2025-02-20 23:42:50 +0100

    UI added, code refactored

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const WEAPONS = ["rock", "paper", "scissors"];
-let humanScore = 0;
-let computerScore = 0;
-let roundNumber = 0;
+let humanScore
+let computerScore
+let roundNumber
+let humanChoice
+
+//UI
+const gameZone = document.querySelector(".game-zone");
+const statusBar = document.querySelector(".status");
+const weaponBtns = document.querySelector(".weapons")
+const weaponBtnsNodeList = document.querySelectorAll(".weapons img")
 
 
-function getHumanChoice(){
-    let choice = "";
-    while(!WEAPONS.includes(choice)){
-        choice = String(prompt("Choose your weapon (rock, paper or scissors)")).toLowerCase()
-        if(!WEAPONS.includes(choice)){
-            console.log(`Wrong weapon ${choice}! Try again`)
-        }
-    }
-    return choice.toLowerCase();
-    
-   
-}
+
+ function getHumanChoice(event){
+    humanChoice = event.target.id
+    playRound(humanChoice, getComputerChoice())
+ }
 
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
@@ -23,41 +23,90 @@ function getComputerChoice(){
 }
 
 function playRound(humanChoice,computerChoice){
-    console.log(`Round ${++roundNumber}`)
-    console.log(`Your weapon: ${humanChoice}`)
-    console.log(`Computer's weapon: ${computerChoice}`)
+
+    gameZone.textContent = ""
+
+    const img1 = document.createElement("img")
+    img1.src = `assets/${humanChoice}.png`
+    const img2 = document.createElement("img")
+    img2.src = `assets/${computerChoice}.png`
+
+    statusBar.textContent = `Round #${++roundNumber}. `
+
+    gameZone.appendChild(img1)
+    gameZone.appendChild(img2)
 
     if(humanChoice === computerChoice){
-        console.log("It is a draw!");
+        statusBar.textContent+= "It is a draw!";
     }
     else if((humanChoice === "rock" && computerChoice === "scissors")
     || (humanChoice === "paper" && computerChoice === "rock")
     || (humanChoice === "scissors" && computerChoice === "paper")){
-        console.log(`You win this round! ${humanChoice} beats ${computerChoice}`)
+        statusBar.textContent+= `You win this round! ${humanChoice} beats ${computerChoice}`
         humanScore++;
     }
     else{
-        console.log(`You lose this round! ${computerChoice} beats ${humanChoice}`)
+        statusBar.textContent += `You lose this round! ${computerChoice} beats ${humanChoice}`
         computerScore++;
     }
-
-    console.log("Current score:")
-    console.log(`You: ${humanScore}`);
-    console.log(`Computer: ${computerScore}`);
-    console.log("===============================");
+    renderScore()
+    checkWinner()
+    
 }
 
+function renderScore(){
+    const humanResultDiv = document.querySelector('.human .result');
+    humanResultDiv.textContent = humanScore
+    const computerResultDiv = document.querySelector('.computer .result');
+    computerResultDiv.textContent = computerScore
+}
 
-function playGame(){
-    while(humanScore < 5 && computerScore < 5){
-        playRound(getHumanChoice(),getComputerChoice());
-    }
+function checkWinner(){
+
     if(humanScore === 5){
-        console.log("You won the Game")
-    }
-    else{
-        console.log("Unfortunately, you lose the Game")
+        stopGame("You won the game!")
+   
+
+    } else if(computerScore === 5){
+        stopGame("Unfortunately, you lose the game")
+
     }
 }
 
-playGame();
+function stopGame(winnerText){
+    
+
+    const button = document.createElement("button")
+    button.textContent = "New Game"
+    weaponBtns.removeEventListener("click", getHumanChoice)
+   
+    weaponBtnsNodeList.forEach((btn) => {
+        btn.classList.toggle("weapon")
+    })
+   
+    setTimeout(() => {
+        gameZone.textContent = winnerText
+        statusBar.textContent = ""
+        statusBar.appendChild(button)
+        button.addEventListener("click", init)
+      }, 2000);
+
+}
+
+function init(){
+    humanScore = 0
+    computerScore = 0
+    roundNumber = 0
+    humanChoice = ""
+    renderScore()
+    weaponBtns.addEventListener("click", getHumanChoice)
+    statusBar.textContent = "Choose your weapon!"
+    gameZone.textContent = "We play up to 5 points"
+    weaponBtnsNodeList.forEach((btn) => {
+        btn.classList.toggle("weapon")
+    })
+
+    
+}
+
+init()
